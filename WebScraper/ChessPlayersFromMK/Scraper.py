5099bc726acd967a4ac8d74e89110ca0640d7963
Martin Andonov <86493954+martinau7@users.noreply.github.com>
2023-04-04 10:01:33 +0200

    Displaying the date scraped in matplotlib.
    Taking the scraped data from the particular web page and displaying it visually how Player rankings and Chess Elo are growing together.

--- WebScraper/ChessPlayersFromMK/Scraper.py
+++ WebScraper/ChessPlayersFromMK/Scraper.py
@@ -1,5 +1,7 @@
 from urllib.request import urlopen
 
+import matplotlib.pyplot as plt
+import pandas as pd
 from bs4 import BeautifulSoup
 
 url = "https://chess-rankings.com/?country=Macedonia"
@@ -10,14 +12,29 @@ allrows = soup.find_all("tbody", {"class": "contenidobusqueda"})
 
 tbody = soup.find('tbody', {'class': 'contenidobusqueda'})
 rows = tbody.find_all('tr')
+data_pname = []
+data_pelo = []
+data_page = []
+data_prank = []
 
 for row in rows:
     player_id = row['id']
     name = row.find('td', {'class': 'nombre'}).text.strip()
     num = row.select('td:nth-of-type(6)')[0].text.strip()
     second_last_td = row.find_all('td')[-2].text.strip()
-    print(f"Player Rank: {player_id}")
-    print(f"Player Name: {name}")
-    print(f"Player Chess Elo: {num}")
-    print(f"Player Age: {second_last_td}")
-    print("_____________________________")
+    data_pname.append(name)
+    data_pelo.append(num)
+    data_page.append(second_last_td)
+    data_prank.append(num)
+
+df = pd.DataFrame({'Name': data_pname, 'Chess Elo': data_pelo, 'Age': data_page, 'Player Ranking': data_prank})
+print(df)
+
+df['Chess Elo'] = pd.to_numeric(df['Chess Elo'], errors='coerce')
+df['Player Ranking'] = pd.to_numeric(df['Player Ranking'], errors='coerce')
+
+plt.scatter(df['Player Ranking'], df['Chess Elo'])
+plt.xlabel('Player Ranking')
+plt.ylabel('Chess Elo')
+plt.title('Player Ranking vs Chess Elo')
+plt.show()
